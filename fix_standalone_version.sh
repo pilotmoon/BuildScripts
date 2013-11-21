320485e6db5a8e0a83fb54cf86aa390ddf961b7f
Nick Moore <nick@pilotmoon.com>
2013-11-21 18:12:57 +0000

    also fix standalone version in dSYM

--- fix_standalone_version.sh
+++ fix_standalone_version.sh
@@ -5,6 +5,7 @@ subtract=$1
 
 # the plist file and key to replace
 plist=${BUILT_PRODUCTS_DIR}/${INFOPLIST_PATH}
+sym_plist="${BUILT_PRODUCTS_DIR}/${FULL_PRODUCT_NAME}.dSYM/Contents/Info.plist"
 key='CFBundleVersion'
 
 ver_num=`expr $CURRENT_PROJECT_VERSION - $subtract`
@@ -12,3 +13,6 @@ ver_num=`expr $CURRENT_PROJECT_VERSION - $subtract`
 # do the replacement
 echo "Setting $key to $ver_num in Info.plist"
 $buddy -c "Set :$key $ver_num" "$plist"
+
+echo "Setting $key to $ver_num in $sym_plist"
+$buddy -c "Set :$key $ver_num" "$sym_plist"
