a8ebdd71e958d1a0b439621806df54ec695d828d
Nick Moore <nick@pilotmoon.com>
2015-10-05 15:14:54 +0100

    remove debug

--- remove_langs.sh
+++ remove_langs.sh
@@ -1,7 +1,6 @@
 # args: foldername allow1 allow2 allow3 ...
 for x in $1/*.lproj; do
 	code=`echo "$x" | sed 's/.*\/\(.*\)\.lproj/\1/' | tr '[:upper:]' '[:lower:]' | tr '_' '-'`
-	echo "$code"
 	allow=0
 	for allowed_code in "${@:2}"; do
 		if [ "$code" == "$allowed_code" ]; then			
