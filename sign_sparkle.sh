65a010d51055d1fe915e75426aaa9ed2c1c947dd
Nick Moore <nick@pilotmoon.com>
2016-02-26 12:30:36 +0000

    Update scripts to use signing identity as a parameter

--- sign_sparkle.sh
+++ sign_sparkle.sh
@@ -1,10 +1,10 @@
 # clean finish_installation
 ./remove_langs.sh "$BUILT_PRODUCTS_DIR/$FRAMEWORKS_FOLDER_PATH/Sparkle.framework/Resources/finish_installation.app/Contents/Resources/" "$@"
 rm -rf "$BUILT_PRODUCTS_DIR/$FRAMEWORKS_FOLDER_PATH/Sparkle.framework/Resources/finish_installation.app/Contents/Resources/Sparkle.icns"
-codesign -s "Developer ID Application: Nicholas Moore" -f "$BUILT_PRODUCTS_DIR/$FRAMEWORKS_FOLDER_PATH/Sparkle.framework/Resources/finish_installation.app"
+codesign -s "$signing_identity" -f "$BUILT_PRODUCTS_DIR/$FRAMEWORKS_FOLDER_PATH/Sparkle.framework/Resources/finish_installation.app"
 
 # clean sparkle
 rm -rf "$BUILT_PRODUCTS_DIR/$FRAMEWORKS_FOLDER_PATH/Sparkle.framework/Headers"
 rm -rf "$BUILT_PRODUCTS_DIR/$FRAMEWORKS_FOLDER_PATH/Sparkle.framework/Versions/Current/Headers"
 ./remove_langs.sh "$BUILT_PRODUCTS_DIR/$FRAMEWORKS_FOLDER_PATH/Sparkle.framework/Resources/" "$@"
-codesign -s "Developer ID Application: Nicholas Moore" -f "$BUILT_PRODUCTS_DIR/$FRAMEWORKS_FOLDER_PATH/Sparkle.framework/Versions/A"
+codesign -s "$signing_identity" -f "$BUILT_PRODUCTS_DIR/$FRAMEWORKS_FOLDER_PATH/Sparkle.framework/Versions/A"
