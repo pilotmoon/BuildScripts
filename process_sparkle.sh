af4af386892d59afaf0cf33c01935525343848ad
Nick Moore <nick@pilotmoon.com>
2019-08-10 12:13:38 +0100

    Remove timestamps from signing

--- process_sparkle.sh
+++ process_sparkle.sh
@@ -1,6 +1,6 @@
 # clean finish_installation
 $nm_root/remove_langs.sh "$BUILT_PRODUCTS_DIR/$FRAMEWORKS_FOLDER_PATH/Sparkle.framework/Resources/finish_installation.app/Contents/Resources/" $nm_allowed_languages
 rm -rf "$BUILT_PRODUCTS_DIR/$FRAMEWORKS_FOLDER_PATH/Sparkle.framework/Resources/finish_installation.app/Contents/Resources/Sparkle.icns"
-codesign -o runtime -s "$nm_signing_identity" -f "$BUILT_PRODUCTS_DIR/$FRAMEWORKS_FOLDER_PATH/Sparkle.framework/Resources/finish_installation.app"
+codesign --timestamp=none -o runtime -s "$nm_signing_identity" -f "$BUILT_PRODUCTS_DIR/$FRAMEWORKS_FOLDER_PATH/Sparkle.framework/Resources/finish_installation.app"
 
 $nm_root/process_framework.sh Sparkle
